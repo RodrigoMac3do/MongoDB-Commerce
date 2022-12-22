9b4b875568c3a46ad72283202a19609399766a15
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-12-22 17:36:48 -0300

    query: lanches com picles e 3 primeiros itens

---
+++ challenges/desafio14.js
@@ -0,0 +1,15 @@
+db.produtos.find(
+  {
+    ingredientes: {
+      $eq: "picles",
+    },
+  },
+  {
+    nome: 1,
+    ingredientes: 1,
+    valoresNutricionais: {
+      $slice: 3,
+    },
+    _id: 0,
+  },
+);
