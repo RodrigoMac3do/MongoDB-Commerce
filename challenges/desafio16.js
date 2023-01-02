eeb467d4b2d1e275b0768b94a2916cac5493d037
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-02 17:11:09 -0300

    query: requisito 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,24 @@
+db.produtos.updateOne(
+  {
+    nome: {
+      $eq: "Big Mac",
+    },
+  },
+  {
+    $set: {
+      ultimaModificacao: Date(),
+    },
+  },
+);
+
+db.produtos.find(
+  {
+    ultimaModificacao: {
+      $exists: true,
+    },
+  },
+  {
+    nome: 1,
+    _id: 0,
+  },
+);
