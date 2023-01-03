3f6d803de44141873ca65dd776820cb8d3fff07b
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-02 23:02:04 -0300

    query: requisito 18

---
+++ challenges/desafio18.js
@@ -0,0 +1,34 @@
+db.produtos.updateMany(
+  {
+    $and: [
+      {
+        $or: [
+          {
+            nome: {
+              $eq: "Big Mac",
+            },
+          },
+          {
+            nome: {
+              $eq: "Quarteirão com Queijo",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  {
+    $push: {
+      ingredientes: "bacon",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    ingredientes: 1,
+  },
+);
