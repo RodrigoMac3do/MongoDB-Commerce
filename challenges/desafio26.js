6c207bb6ce04553480a5be3c45f2c226ecaa9ec2
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-03 20:02:55 -0300

    query: requisito 26

---
+++ challenges/desafio26.js
@@ -0,0 +1,37 @@
+db.produtos.updateMany(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "sódio",
+        $and: [
+          {
+            percentual: {
+              $gt: 20,
+            },
+          },
+          {
+            percentual: {
+              $lt: 40,
+            },
+          },
+        ],
+      },
+    },
+  },
+  {
+    $push: {
+      tags: {
+        $each: ["contém sódio"],
+      },
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    tags: 1,
+  },
+);
