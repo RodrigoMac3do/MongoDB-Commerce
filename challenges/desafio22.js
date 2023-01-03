57a0017ae38211e594fc674fe99e63d2aa838ef6
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-03 17:20:21 -0300

    query: requisito 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,43 @@
+db.produtos.updateMany(
+  {},
+  {
+    $set: {
+      vendasPorDia: [0, 0, 0, 0, 0, 0, 0],
+    },
+  },
+);
+
+db.produtos.updateOne(
+  {
+    nome: {
+      $eq: "Big Mac",
+    },
+  },
+  {
+    $set: {
+      "vendasPorDia.3": 60,
+    },
+  },
+);
+
+db.produtos.updateMany(
+  {
+    tags: {
+      $eq: "bovino",
+    },
+  },
+  {
+    $set: {
+      "vendasPorDia.6": 120,
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    vendasPorDia: 1,
+  },
+);
