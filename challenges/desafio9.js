f9266c4f67d1453e28865c23453afb36d02b7733
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-06 20:53:58 -0300

    refactor:  filtragem usando $elemMatch

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,11 +1,16 @@
 db.produtos.find(
   {
-    "valoresNutricionais.0.quantidade": {
-      $lt: 500,
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: 'calorias',
+        quantidade: {
+          $lt: 500,
+        },
+      },
     },
   },
   {
     nome: 1,
     _id: 0,
-  },
+  }
 );
